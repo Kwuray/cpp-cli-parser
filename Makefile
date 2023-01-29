8ab90eb184c8a986d5faee06a8dc6c0b597efba8
Kwuray <nicolas@iMac-de-Nicolas.lan>
2023-01-29 17:43:26 +0100

    v0

---
+++ Makefile
@@ -0,0 +1,32 @@
+CC = clang++
+PARAMS = -std=c++20 -Wall
+
+EXECUTABLE_FOLDER = bin/exec
+EXECUTABLE_NAME = cliparser
+EXECUTABLE_PATH = $(EXECUTABLE_FOLDER)/$(EXECUTABLE_NAME)
+
+##################################################
+#Main files
+MAIN_SRC_PATH = ./
+MAIN_OBJ_PATH = bin/
+SRC_FILES_MAIN = $(wildcard $(MAIN_SRC_PATH)*.cpp)
+OBJ_FILES_MAIN = $(SRC_FILES_MAIN:$(MAIN_SRC_PATH)%.cpp=$(MAIN_OBJ_PATH)%.o)
+#recipes
+$(MAIN_OBJ_PATH)%.o : $(MAIN_SRC_PATH)%.cpp $(MAIN_SRC_PATH)%.h
+	$(CC) -o $@ -c $< $(PARAMS)
+
+$(MAIN_OBJ_PATH)main.o : $(MAIN_SRC_PATH)main.cpp
+	$(CC) -o $@ -c $< $(PARAMS)
+#Main files
+##################################################
+
+build : $(OBJ_FILES_MAIN) $(EXECUTABLE_PATH)
+
+$(EXECUTABLE_PATH) : $(OBJ_FILES_MAIN)
+	$(CC) -o $@ $^ $(PARAMS)
+
+.PHONY: clear
+
+clear:
+	rm -f bin/*.o
+	rm -f $(EXECUTABLE_PATH)
---
+++ option.cpp
@@ -0,0 +1,53 @@
+#include "option.h"
+#include <string>
+using namespace std;
+
+//constructor -- short option
+Option::Option(char shortOption, string description, bool optional, bool value) {
+  this->shortOption = shortOption;
+  this->description = description;
+  this->setOptional(optional);
+  this->type = value ? OPTION_SHORT_VALUE : OPTION_SHORT;
+}
+
+//constructor -- long option
+Option::Option(string longOption, string description, bool optional, bool value) {
+  this->longOption = longOption;
+  this->description = description;
+  this->setOptional(optional);
+  this->type = value ? OPTION_LONG_VALUE : OPTION_LONG;
+}
+
+//constructor -- short and long option
+Option::Option(char shortOption, string longOption, string description, bool optional, bool value) {
+  this->shortOption = shortOption;
+  this->longOption = longOption;
+  this->description = description;
+  this->setOptional(optional);
+  this->type = value ? OPTION_SHORT_LONG_VALUE : OPTION_SHORT_LONG;
+}
+
+//getter type
+optionType Option::getType() {
+  return this->type;
+}
+
+//getter shortOption
+char Option::getShortOption() {
+  return this->shortOption;
+}
+
+//getter shortOption
+string Option::getLongOption() {
+  return this->longOption;
+}
+
+//setter optional
+void Option::setOptional(bool value) {
+  this->optional = value;
+}
+
+//setter optional
+bool Option::getOptional() {
+  return this->optional;
+}
